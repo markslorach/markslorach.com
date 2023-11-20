ac865fb6473fa9199103d6f9153c03a0795355f1
Mark Slorach <hello@markslorach.com>
2023-11-20 14:04:43 +0000

    updated project page

--- app/(site)/projects/[project]/page.jsx
+++ app/(site)/projects/[project]/page.jsx
@@ -22,17 +22,8 @@ export default async function Project({ params }) {
 
   return (
     <main>
-      <figure className="mb-4">
-        <ImageCarousel images={project.images} />
-      </figure>
-
-      <div className="flex flex-wrap gap-4 text-white/50 text-xs tracking-wide leading-none font-mono mb-16">
-        {project.techStack.map((tech, index) => (
-          <p key={index}>{tech}</p>
-        ))}
-      </div>
-      <div className="flex gap-3 items-center">
-        <h2 className="">{project.title}</h2>
+      <div className="flex gap-3 justify-between items-center mb-5">
+        <h2>{project.title}</h2>
         <button className="text-white/70 hover:text-white/90 hover:scale-105 transition-all duration-300 ease-in-out">
           <Link
             href={project.github}
@@ -43,7 +34,18 @@ export default async function Project({ params }) {
           </Link>
         </button>
       </div>
-
+      <figure className="mb-16">
+        <ImageCarousel images={project.images} />
+      </figure>
+      <div className="mb-16">
+        <h2 className="mb-5">Tech Stack</h2>
+        <div className="flex flex-wrap gap-4 text-white/50 text-sm tracking-wide leading-none">
+          {project.techStack.map((tech, index) => (
+            <p key={index}>{tech}</p>
+          ))}
+        </div>
+      </div>
+      <h2>Project Info</h2>
       <PortableText value={project.content} components={RichTextComponents} />
     </main>
   );
