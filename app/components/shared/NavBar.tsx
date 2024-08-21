330d823141a3f75867fc93ab8e12c505a9d75412
Mark Slorach <hello@markslorach.com>
2024-08-21 12:18:35 +0100

    setup, project data and project interface

---
+++ app/components/shared/NavBar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between py-10">
+      <h1>markslorach.</h1>
+      <span>toggle</span>
+    </nav>
+  );
+};
+
+export default NavBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/shared/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="mx-auto max-w-2xl px-4">
+          <NavBar />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
