d395153f643a4046d2ae086aea0fba35aa7e3715
Mark Slorach <hello@markslorach.com>
2025-01-15 17:45:28 +0000

    updated footer

--- app/components/shared/Footer.tsx
+++ app/components/shared/Footer.tsx
@@ -4,17 +4,9 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="mt-24 flex items-center justify-between gap-4 border-t py-8">
-      <span className="text-lg text-neutral-800/60">
-        created by{" "}
-        <Link
-          className="font-semibold text-blue-400 transition-colors hover:text-blue-500"
-          href="https://linkedin.com/in/markslorach"
-          target="_blank"
-          aria-label="Link for Mark Slorach's LinkedIn profile"
-        >
-          mark slorach
-        </Link>
-        .
+      <span className="text-sm text-neutral-800/60">
+        Made with{" "}
+        <span role="img" aria-label="heart">❤️</span> using Next.js and Shadcn/ui.
       </span>
       <Link
         href="https://github.com/markslorach/markslorach.com.git"
